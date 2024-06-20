57e4897df30c463ecce8ee4379fbbaef9b7b041e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-19 22:30:24 -0700

    Update mod.rs

--- src/services/products/mod.rs
+++ src/services/products/mod.rs
@@ -3,7 +3,10 @@ mod models;
 use models::Product;
 use serde::{Deserialize, Serialize};
 use surrealdb::{engine::local::Db, Surreal};
-use swd::{async_graphql::Context, Object};
+use swd::{
+    async_graphql::{types::connection::*, Context},
+    Object,
+};
 
 #[derive(Default)]
 pub struct ProductsQuery;
@@ -13,14 +16,42 @@ pub struct ProductsMutation;
 
 #[Object]
 impl ProductsQuery {
-    async fn status<'ctx>(&self, ctx: &Context<'ctx>) -> &str {
+    async fn status<'ctx>(&self, ctx: &Context<'ctx>, id: String) -> &str {
         let db = ctx.data::<Surreal<Db>>();
         "Server Is Running OK...!"
     }
 
-    async fn status<'ctx>(&self, ctx: &Context<'ctx>) -> &str {
-        let db = ctx.data::<Surreal<Db>>();
-        "Server Is Running OK...!"
+    async fn numbers(
+        &self,
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<i32>,
+        last: Option<i32>,
+    ) -> Result<Connection<usize, i32, EmptyFields, EmptyFields>> {
+        query(
+            after,
+            before,
+            first,
+            last,
+            |after, before, first, last| async move {
+                let mut start = after.map(|after| after + 1).unwrap_or(0);
+                let mut end = before.unwrap_or(10000);
+                if let Some(first) = first {
+                    end = (start + first).min(end);
+                }
+                if let Some(last) = last {
+                    start = if last > end - start { end } else { end - last };
+                }
+                let mut connection = Connection::new(start > 0, end < 10000);
+                connection.edges.extend(
+                    (start..end)
+                        .into_iter()
+                        .map(|n| Edge::with_additional_fields(n, n as i32, EmptyFields)),
+                );
+                Ok::<_, async_graphql::Error>(connection)
+            },
+        )
+        .await
     }
 }
 
