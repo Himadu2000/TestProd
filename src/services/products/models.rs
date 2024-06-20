fc056bb831d7ac8104a747d124071a0ed82bc89f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-19 21:17:37 -0700

    Update models.rs

--- src/services/products/models.rs
+++ src/services/products/models.rs
@@ -2,9 +2,8 @@ use serde::{Deserialize, Serialize};
 use surrealdb::sql::{Bytes, Thing};
 
 #[derive(Debug, Serialize)]
-pub struct Product<'a> {
-    first: &'a str,
-    last: &'a str,
+pub struct Product {
+    first: String,
 }
 
 #[derive(Debug, Deserialize)]
