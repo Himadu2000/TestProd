1281fe4a3095e4abfaff97ec3bfbf64d5777b04d
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-19 19:56:30 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use crate::{
     services::{Mutation, Query},
     util::graphql::graphql,
 };
+use serde::{Deserialize, Serialize};
+use surrealdb::engine::local::Mem;
+use surrealdb::sql::Thing;
+use surrealdb::Surreal;
 use swd::{
     async_graphql::{EmptySubscription, Schema},
     index,
@@ -14,6 +18,12 @@ use swd::{
 
 #[launch]
 fn rocket() -> _ {
+    // Create database connection
+    let db = Surreal::new::<Mem>(()).await?;
+
+    // Select a specific namespace / database
+    db.use_ns("test").use_db("test").await?;
+
     let schema = Schema::build(Query::default(), Mutation::default(), EmptySubscription).finish();
 
     build()
