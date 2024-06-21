37c7b3f53370c2dd6b2dc4c7ae28ea2c8b5d1202
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-21 05:19:22 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -32,20 +32,6 @@ async fn rocket() -> _ {
 
     build()
         .attach(Cors)
-        .attach(AdHoc::on_response(
-            "Set Authorization and store_id headers to response!",
-            |req, res| {
-                Box::pin(async move {
-                    let headers = req.headers();
-
-                    let authorization = headers.get_one("Authorization").unwrap_or_default();
-                    let store_id = headers.get_one("store_id").unwrap_or_default();
-
-                    res.adjoin_header(Header::new(AUTHORIZATION.as_str(), authorization));
-                    res.adjoin_header(Header::new("store_id", store_id));
-                })
-            },
-        ))
         .manage(schema)
         .mount("/", routes![index])
         .mount("/graphql", graphql())
