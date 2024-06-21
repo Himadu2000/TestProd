8e150686fb584045d5e431718e6fb50761d68cdf
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-21 05:09:10 -0700

    Update graphql.rs

--- src/util/graphql.rs
+++ src/util/graphql.rs
@@ -2,7 +2,11 @@ use crate::services::{Mutation, Query};
 use swd::{
     async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse},
     graphiql,
-    rocket::{get, post, routes, Route},
+    rocket::{
+        async_trait, get, post,
+        request::{FromRequest, Outcome, Request},
+        routes, Route,
+    },
     GQLSchema,
 };
 
@@ -13,9 +17,35 @@ async fn graphql_query(schema: &GraphqlSchema, query: GraphQLQuery) -> GraphQLRe
     query.execute(schema.inner()).await
 }
 
+pub struct Headers {
+    pub authorization: String,
+    pub store_id: String,
+}
+
+#[async_trait]
+impl<'r> FromRequest<'r> for Headers {
+    type Error = String;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let headers = req.headers();
+
+        let authorization = headers.get_one("Authorization").unwrap_or_default();
+        let store_id = headers.get_one("store_id").unwrap_or_default();
+
+        Outcome::Success(Headers {
+            authorization: authorization.to_owned(),
+            store_id: store_id.to_owned(),
+        })
+    }
+}
+
 #[post("/", data = "<request>", format = "application/json")]
-async fn graphql_request(schema: &GraphqlSchema, request: GraphQLRequest) -> GraphQLResponse {
-    request.execute(schema.inner()).await
+async fn graphql_request(
+    schema: &GraphqlSchema,
+    request: GraphQLRequest,
+    headers: Headers,
+) -> GraphQLResponse {
+    request.data(headers).execute(schema.inner()).await
 }
 
 pub fn graphql() -> impl Into<Vec<Route>> {
