3da3b0cd836d6262947def83d81cb9d32d798905
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-21 00:20:41 -0700

    Update models.rs

--- src/services/products/models.rs
+++ src/services/products/models.rs
@@ -171,3 +171,43 @@ impl ProductRecord {
         format!("{}:{}", &self.id.tb, &self.id.id)
     }
 }
+
+#[derive(Clone, Copy, Enum, Eq, PartialEq, Serialize, Deserialize)]
+pub enum ProductFilterStockStatus {
+    AVAILABLE,
+}
+
+#[derive(Clone, Copy, Enum, Eq, PartialEq, Serialize, Deserialize)]
+pub enum ProductFilterSort {
+    PriceMinToMax,
+    PriceMaxToMin,
+    StockHighest,
+    StockLowest,
+}
+
+#[derive(InputObject)]
+#[graphql(complex)]
+pub struct Filter {
+    #[allow(dead_code)]
+    #[graphql(skip)]
+    category_id: Option<Thing>,
+    active: Option<bool>,
+    discontinued: Option<bool>,
+    search: Option<String>,
+    on_sale: Option<bool>,
+    stock_status: ProductFilterStockStatus,
+    price_from: Option<f32>,
+    price_to: Option<f32>,
+    sku: Option<String>,
+    #[allow(dead_code)]
+    #[graphql(skip)]
+    ids: Option<Vec<Thing>>,
+    sort: Option<ProductFilterSort>,
+}
+
+#[ComplexObject]
+impl Filter {
+    async fn id(&self) -> String {
+        String::new()
+    }
+}
