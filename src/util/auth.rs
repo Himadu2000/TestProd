e4c54ed7b4e6157d0c858798c73a24ae968db305
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-21 01:57:23 -0700

    Update auth.rs

--- src/util/auth.rs
+++ src/util/auth.rs
@@ -5,18 +5,7 @@ use surrealdb::{engine::local::Db, Surreal};
 use swd::{async_graphql::Context, Object};
 
 #[derive(Default)]
-pub struct AuthQuery;
-
-#[derive(Default)]
 pub struct AuthMutation;
-
-#[Object]
-impl AuthQuery {
-    async fn status<'ctx>(&self, ctx: &Context<'ctx>) -> &str {
-        let db = ctx.data::<Surreal<Db>>();
-        "Server Is Running OK...!"
-    }
-}
 
 #[Object]
 impl AuthMutation {
