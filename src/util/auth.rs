4764dde618f07dd491d26cd8c5e59ed1d4441356
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-21 02:37:16 -0700

    Update auth.rs

--- src/util/auth.rs
+++ src/util/auth.rs
@@ -15,7 +15,7 @@ pub struct AuthMutation;
 impl AuthMutation {
     async fn login(&self, #[graphql(validator(email))] email: String) -> Result<String, &str> {
         if email == String::from("03124701209@gmail.com") {
-            return Ok(String::from("token03124701209"));
+            return Ok(String::from("Bearer token03124701209"));
         }
 
         Err("Not given access...!")
