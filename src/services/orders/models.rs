616d77a8c1918c0fe771b650a30dab40fdeaf5d3
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-20 23:38:21 -0700

    Update models.rs

--- src/services/orders/models.rs
+++ src/services/orders/models.rs
@@ -61,12 +61,55 @@ impl Variant {
 #[derive(Default, Serialize, Deserialize, SimpleObject, InputObject)]
 #[graphql(complex, input_name = "OrderInput")]
 pub struct Order {
-    pub name: Option<String>,
-    pub description: Option<String>,
-    pub meta_description: Option<String>,
-    pub meta_title: Option<String>,
-    #[graphql(default)]
-    pub tags: Vec<String>,
+    #[graphql(skip)]
+    date_placed: Datetime,
+    #[graphql(skip)]
+    date_closed: Option<Datetime>,
+    #[graphql(skip)]
+    date_paid: Option<Datetime>,
+    #[graphql(skip)]
+    date_cancelled: Option<Datetime>,
+    number: u16,
+    shipping_status: String,
+    items: Vec<Item>,
+    transactions: Vec<String>,
+    discounts: Vec<String>,
+    billing_address: Address,
+    shipping_address: Address,
+    tax_rate: f32,
+    shipping_tax: f32,
+    shipping_discount: f32,
+    shipping_price: f32,
+    item_tax_included: bool,
+    shipping_tax_included: bool,
+    closed: bool,
+    cancelled: bool,
+    delivered: bool,
+    paid: bool,
+    hold: bool,
+    draft: bool,
+    first_name: String,
+    last_name: String,
+    password: Option<String>,
+    email: String,
+    mobile: u16,
+    referrer_url: String,
+    landing_url: String,
+    channel: String,
+    note: String,
+    comments: String,
+    #[graphql(skip)]
+    coupon: Option<Thing>,
+    tracking_number: u16,
+    #[graphql(skip)]
+    customer_id: Thing,
+    #[graphql(skip)]
+    status_id: Thing,
+    #[graphql(skip)]
+    payment_method_id: Thing,
+    #[graphql(skip)]
+    shipping_method_id: Thing,
+    tags: Vec<String>,
 }
 
 #[ComplexObject]
