61754cb0c5e0a7f6d06d822b4430dd00d5fae1f5
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-25 00:49:51 -0700

    Update mod.rs

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,11 +6,14 @@ pub mod store;
 use graphql::Headers;
 use swd::{async_graphql::Context, async_graphql::Error, SurrealDb};
 
-pub async fn db_and_headers<'ctx>(ctx: &Context<'ctx>, _scope: String) -> Result<(), &'static str> {
+pub fn db_and_headers<'ctx>(
+    ctx: &Context<'ctx>,
+    _scope: String,
+) -> Result<(&'ctx SurrealDb, Headers), &'static str> {
     let db = ctx.data::<SurrealDb>().map_err(error)?;
     let headers = ctx.data::<Headers>().map_err(error)?;
 
-    (db, headers)
+    Ok((db, headers))
 }
 
 pub fn error(_: Error) -> &'static str {
