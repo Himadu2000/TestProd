0690cdd9b80d8c0f783abca757c5679258156214
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-24 21:37:55 -0700

    Adding stores

---
+++ src/services/stores/models.rs
@@ -0,0 +1,61 @@
+use swd::{ComplexObject, Deserialize, Enum, InputObject, Serialize, SimpleObject, Thing};
+
+#[derive(Clone, Copy, Default, Enum, Eq, PartialEq, Serialize, Deserialize)]
+pub enum ProductOptionControl {
+    #[default]
+    SELECT,
+}
+
+#[derive(Serialize, Deserialize, SimpleObject, InputObject)]
+#[graphql(complex, input_name = "VariantInput")]
+pub struct Variant {
+    pub sku: Option<String>,
+    pub price: f32,
+    pub stock_quantity: u16,
+    pub weight: Option<f32>,
+    #[graphql(skip)]
+    pub options: Vec<VariantOption>,
+}
+
+#[ComplexObject]
+impl Variant {
+    async fn options(&self) -> String {
+        String::new()
+    }
+}
+
+#[derive(Default, Serialize, Deserialize, SimpleObject, InputObject)]
+#[graphql(complex, input_name = "ProductInput")]
+pub struct Product {
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub meta_description: Option<String>,
+    pub meta_title: Option<String>,
+    #[graphql(default)]
+    pub tags: Vec<String>,
+}
+
+#[ComplexObject]
+impl Product {
+    async fn date_stock_expected(&self) -> String {
+        String::new()
+    }
+}
+
+#[derive(Deserialize, SimpleObject)]
+#[graphql(complex)]
+pub struct ProductRecord {
+    #[allow(dead_code)]
+    #[graphql(skip)]
+    pub id: Thing,
+    #[serde(flatten)]
+    #[graphql(flatten)]
+    pub product: Product,
+}
+
+#[ComplexObject]
+impl ProductRecord {
+    async fn id(&self) -> String {
+        format!("{}:{}", &self.id.tb, &self.id.id)
+    }
+}
