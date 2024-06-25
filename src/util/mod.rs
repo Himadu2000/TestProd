99b90fbf455d57cb23fd17727c1ff3df0774e337
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-25 00:45:57 -0700

    Update mod.rs

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,7 +3,27 @@ pub mod files;
 pub mod graphql;
 pub mod store;
 
+use super::graphql::Headers;
 use swd::async_graphql::Error;
+use swd::{async_graphql::Context, Object};
+
+pub async fn is_authorized<'ctx>(ctx: &Context<'ctx>, _scope: String) -> Result<(), &'static str> {
+    let token = ctx
+        .insert_http_header("Authorization", "")
+        .ok_or("Authorization header not set...!")?;
+
+    let token = token
+        .to_str()
+        .map_err(|_| "Incorrect Authorization header...!")?;
+
+    let headers = ctx.data::<Headers>().unwrap();
+
+    if headers.authorization == "Bearer token03124701209" {
+        return Ok(());
+    }
+
+    Err("Not authorized...!")
+}
 
 pub fn error(_: Error) -> &'static str {
     "Connection error...!"
