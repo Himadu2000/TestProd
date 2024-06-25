0d9ae4bfe6bd79ee23c5556cc218c60d7879a4bd
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-24 23:20:00 -0700

    Create models.rs

---
+++ src/util/store/models.rs
@@ -0,0 +1,41 @@
+use swd::{ComplexObject, Deserialize, InputObject, Serialize, SimpleObject, Thing};
+
+#[derive(Default, Serialize, Deserialize, SimpleObject, InputObject)]
+#[graphql(complex, input_name = "StoresInput")]
+pub struct Stores {
+    #[graphql(default = "Untitled")]
+    pub name: String,
+    #[graphql(skip)]
+    pub users: Vec<Thing>,
+    #[graphql(skip)]
+    pub products: Vec<Thing>,
+}
+
+#[ComplexObject]
+impl Stores {
+    async fn users(&self) -> String {
+        String::new()
+    }
+
+    async fn products(&self) -> String {
+        String::new()
+    }
+}
+
+#[derive(Deserialize, SimpleObject)]
+#[graphql(complex)]
+pub struct StoresRecord {
+    #[allow(dead_code)]
+    #[graphql(skip)]
+    pub id: Thing,
+    #[serde(flatten)]
+    #[graphql(flatten)]
+    pub stores: Stores,
+}
+
+#[ComplexObject]
+impl StoresRecord {
+    async fn id(&self) -> String {
+        format!("{}:{}", &self.id.tb, &self.id.id)
+    }
+}
