81fcad0124533dc0dfadd83f076d0b74ceac3e0c
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-24 21:44:40 -0700

    Update mod.rs

--- src/services/stores/mod.rs
+++ src/services/stores/mod.rs
@@ -1,6 +1,6 @@
 mod models;
 
-use models::Product;
+use models::Stores;
 use surrealdb::{engine::local::Db, Surreal};
 use swd::{async_graphql::Context, Object};
 
@@ -20,7 +20,47 @@ impl StoresQuery {
 
 #[Object]
 impl StoresMutation {
-    async fn status(&self) -> &str {
-        "Server Is Running OK...!"
+    async fn create_product<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        data: Product,
+    ) -> Result<Vec<ProductRecord>, &str> {
+        is_authorized(ctx, String::new()).await?;
+
+        let db = ctx.data::<SurrealDb>().unwrap();
+
+        let products: Vec<ProductRecord> = db.create("product").content(data).await.unwrap();
+
+        Ok(products)
+    }
+
+    async fn update_product<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        id: String,
+        data: Product,
+    ) -> Result<Option<ProductRecord>, &str> {
+        is_authorized(ctx, String::new()).await?;
+
+        let db = ctx.data::<SurrealDb>().unwrap();
+
+        let product: Option<ProductRecord> = db.update(("product", id)).merge(data).await.unwrap();
+
+        Ok(product)
+    }
+
+    async fn delete_product<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        id: String,
+        delete_image_index: Option<u8>,
+    ) -> Result<Option<ProductRecord>, &str> {
+        is_authorized(ctx, String::new()).await?;
+
+        let db = ctx.data::<SurrealDb>().unwrap();
+
+        let product: Option<ProductRecord> = db.delete(("product", id)).await.unwrap();
+
+        Ok(product)
     }
 }
