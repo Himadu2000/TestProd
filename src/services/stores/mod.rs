079e96716f8d7a40e6d89872f86e3fe2c182b857
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-25 01:33:09 -0700

    Update mod.rs

--- src/services/stores/mod.rs
+++ src/services/stores/mod.rs
@@ -12,7 +12,7 @@ pub struct StoresMutation;
 
 #[Object]
 impl StoresQuery {
-    async fn status<'ctx>(&self, ctx: &Context<'ctx>) -> &str {
+    async fn stores<'ctx>(&self, ctx: &Context<'ctx>) -> &str {
         let db = ctx.data::<Surreal<Db>>();
         "Server Is Running OK...!"
     }
@@ -23,44 +23,19 @@ impl StoresMutation {
     async fn create_product<'ctx>(
         &self,
         ctx: &Context<'ctx>,
-        data: Product,
+        data: Stores,
     ) -> Result<Vec<ProductRecord>, &str> {
         is_authorized(ctx, String::new()).await?;
 
-        let db = ctx.data::<SurrealDb>().unwrap();
+        let (db, store_id) = db_and_store_id(ctx)?;
+
+        let data = ProductDbRecord {
+            store_id,
+            product: data,
+        };
 
         let products: Vec<ProductRecord> = db.create("product").content(data).await.unwrap();
 
         Ok(products)
-    }
-
-    async fn update_product<'ctx>(
-        &self,
-        ctx: &Context<'ctx>,
-        id: String,
-        data: Product,
-    ) -> Result<Option<ProductRecord>, &str> {
-        is_authorized(ctx, String::new()).await?;
-
-        let db = ctx.data::<SurrealDb>().unwrap();
-
-        let product: Option<ProductRecord> = db.update(("product", id)).merge(data).await.unwrap();
-
-        Ok(product)
-    }
-
-    async fn delete_product<'ctx>(
-        &self,
-        ctx: &Context<'ctx>,
-        id: String,
-        delete_image_index: Option<u8>,
-    ) -> Result<Option<ProductRecord>, &str> {
-        is_authorized(ctx, String::new()).await?;
-
-        let db = ctx.data::<SurrealDb>().unwrap();
-
-        let product: Option<ProductRecord> = db.delete(("product", id)).await.unwrap();
-
-        Ok(product)
     }
 }
