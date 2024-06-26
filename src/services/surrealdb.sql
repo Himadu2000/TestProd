0d14c189ef911a793443a3fbff4438f101eb8781
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-26 10:38:38 -0700

    Update surrealdb.sql

--- src/services/surrealdb.sql
+++ src/services/surrealdb.sql
@@ -1,7 +1,7 @@
 -- Namespace for CHost
 DEFINE NAMESPACE CHost;
 -- Specify the namespace for the database
-USE NS abcum;
+USE NS CHost;
 -- Define database
 DEFINE DATABASE Cezerin;
 -- Create a schemafull store table.
