b284cedfe4da79fb572050ebf57c6c336c4a73f2
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-26 10:37:54 -0700

    Update surrealdb.sql

--- src/services/surrealdb.sql
+++ src/services/surrealdb.sql
@@ -6,3 +6,8 @@ USE NS abcum;
 DEFINE DATABASE Cezerin;
 -- Create a schemafull store table.
 DEFINE TABLE store SCHEMAFULL;
+-- Define some fields.
+DEFINE FIELD firstName ON TABLE store TYPE string;
+DEFINE FIELD lastName ON TABLE store TYPE string;
+DEFINE FIELD email ON TABLE store TYPE string;
+ASSERT string::is::email($value);
