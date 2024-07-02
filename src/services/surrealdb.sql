7f21ca3e801fd17db82d3e399bafbe5dba8b8d30
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-01 23:44:36 -0700

    Update surrealdb.sql

--- src/services/surrealdb.sql
+++ src/services/surrealdb.sql
@@ -13,3 +13,8 @@ DEFINE FIELD email ON TABLE store TYPE string;
 ASSERT string::is::email($value);
 -- Create a schemafull product table.
 DEFINE TABLE product SCHEMAFULL;
+-- Define some fields.
+DEFINE FIELD firstName ON TABLE product TYPE string;
+DEFINE FIELD lastName ON TABLE product TYPE string;
+DEFINE FIELD email ON TABLE product TYPE string;
+ASSERT string::is::email($value);
