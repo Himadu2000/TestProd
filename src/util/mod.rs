d649631d035716416c96c2eef61771fa45d83fa1
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-02 04:55:27 -0700

    Update mod.rs

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -64,11 +64,11 @@ pub struct DbInfo {
 pub fn get_db() -> DbInfo {
     let db = var("").unwrap_or("default");
 
-    let (url, db) = db.split_once("://").unwrap_or(("", ""));
-    let (user, db) = db.split_once("://").unwrap_or(("", ""));
-    let (pass, db) = db.split_once("://").unwrap_or(("", ""));
-    let (ns, db) = db.split_once("://").unwrap_or(("", ""));
-    let (db, _) = db.split_once("://").unwrap_or(("", ""));
+    let (_, db) = db.split_once("://").unwrap_or(("", ""));
+    let (user, db) = db.split_once(':').unwrap_or(("", ""));
+    let (pass, db) = db.split_once('@').unwrap_or(("", ""));
+    let (ns, db) = db.split_once('/').unwrap_or(("", ""));
+    let (db, _) = db.split_once('/').unwrap_or(("", ""));
 
     DbInfo {
         url,
