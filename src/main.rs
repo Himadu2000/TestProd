d3b73123571b57d0cfdbcb57f59200a8c90bfba1
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-12 12:27:34 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,44 @@
-mod services;
-mod util;
+// mod services;
+// mod util;
 
-use crate::{
-    services::{Mutation, Query},
-    util::{files::files, get_db, graphql::graphql},
-};
+// use crate::{
+//     services::{Mutation, Query},
+//     util::{files::files, get_db, graphql::graphql},
+// };
 use std::net::{IpAddr, Ipv4Addr};
 use swd::{
-    async_graphql::{EmptySubscription, Schema},
+    // async_graphql::{EmptySubscription, Schema},
     index,
     rocket::{
         build,
         data::{Limits, ToByteUnit},
         launch, routes, Config,
     },
-    surrealdb::{engine::remote::http::Http, opt::auth::Root, Surreal},
+    // surrealdb::{engine::remote::http::Http, opt::auth::Root, Surreal},
     Cors,
 };
 
 #[launch]
 async fn rocket() -> _ {
-    let db_url = get_db();
+    // let db_url = get_db();
 
-    // Create database connection
-    let db = Surreal::new::<Http>(db_url.url).await.unwrap();
+    // // Create database connection
+    // let db = Surreal::new::<Http>(db_url.url).await.unwrap();
 
-    // Sign in as root
-    db.signin(Root {
-        username: &db_url.user,
-        password: &db_url.pass,
-    })
-    .await
-    .unwrap();
+    // // Sign in as root
+    // db.signin(Root {
+    //     username: &db_url.user,
+    //     password: &db_url.pass,
+    // })
+    // .await
+    // .unwrap();
 
-    // Select a specific namespace / database
-    db.use_ns(db_url.ns).use_db(db_url.db).await.unwrap();
+    // // Select a specific namespace / database
+    // db.use_ns(db_url.ns).use_db(db_url.db).await.unwrap();
 
-    let schema = Schema::build(Query::default(), Mutation::default(), EmptySubscription)
-        .data(db.clone())
-        .finish();
+    // let schema = Schema::build(Query::default(), Mutation::default(), EmptySubscription)
+    //     .data(db.clone())
+    //     .finish();
 
     build()
         .configure(Config {
@@ -47,8 +47,8 @@ async fn rocket() -> _ {
             ..Default::default()
         })
         .attach(Cors)
-        .manage(schema)
-        .manage(db)
-        .mount("/", routes![index, files])
-        .mount("/graphql", graphql())
+        // .manage(schema)
+        // .manage(db)
+        .mount("/", routes![index])
+        // .mount("/graphql", graphql())
 }
